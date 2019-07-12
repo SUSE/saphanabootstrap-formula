dfbf20e53a2ea112dadd717660d58b4003408be1
Xabier Arbulu Insausti <xarbulu@suse.com>
2019-07-12 14:53:56 +0100

    Add trick to mock shaptools import in the ci

--- ci/validate-formula.sh
+++ ci/validate-formula.sh
@@ -23,7 +23,7 @@ EOF
 
 sudo salt-call state.show_highstate --local --file-root=./ --config-dir=. --pillar-root=example/pillar --retcode-passthrough -l debug
 
-echo 
+echo
 echo "==========================================="
 echo " Using secondary host                      "
 echo "==========================================="
@@ -37,5 +37,17 @@ cat >minion <<EOF
 root_dir: $PWD
 id: travis
 EOF
+
+# A trick to mock 'from shaptools import hana'
+PYTHON_SITEPACKAGES=$(python -c "import site; print(site.getsitepackages()[1])")
+sudo mkdir $PYTHON_SITEPACKAGES/shaptools
+sudo touch $PYTHON_SITEPACKAGES/shaptools/__init__.py
+sudo touch $PYTHON_SITEPACKAGES/shaptools/hana.py
+sudo sh -c "echo \"
+class HanaInstance(object):
+    @classmethod
+    def get_platform(cls):
+        return 'test_arch'
+\" > $PYTHON_SITEPACKAGES/shaptools/hana.py"
 
 sudo salt-call state.show_highstate --local --file-root=./ --config-dir=. --pillar-root=example/pillar --retcode-passthrough -l debug
